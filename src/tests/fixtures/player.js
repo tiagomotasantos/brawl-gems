400d24cfb2f14f878a4ea65858dfd81597c0bdb7
Tiago Mota Santos <tiago.a.mota@hotmail.com>
2020-01-25 15:21:12 +0000

    test: add test player fixture

---
+++ src/tests/fixtures/player.js
@@ -0,0 +1,246 @@
+export default {
+    "id": {
+      "high": 5,
+      "low": 21339850
+    },
+    "tag": "9RCQGYC8Y",
+    "name": "Mottyze",
+    "nameColorId": 43000006,
+    "nameColorCode": "f05637",
+    "trophies": 6262,
+    "highestTrophies": 6277,
+    "highestPowerPlayPoints": 316,
+    "expLevel": 59,
+    "totalExp": 19252,
+    "expFmt": "402/620",
+    "victories": 830,
+    "soloShowdownVictories": 62,
+    "duoShowdownVictories": 128,
+    "bestRoboRumbleTimeInSeconds": 264,
+    "bestRoboRumbleTime": "4m 24s",
+    "bestTimeAsBigBrawlerInSeconds": 173,
+    "bestTimeAsBigBrawler": "2m 53s",
+    "hasClub": true,
+    "club": {
+      "id": {
+        "high": 9,
+        "low": 154351
+      },
+      "tag": "Y9LG2PV",
+      "name": "LEGIÃO PT",
+      "role": "Member",
+      "badgeId": 0,
+      "badgeUrl": "https://www.starlist.pro/img/club/0.png",
+      "members": 1,
+      "trophies": 0,
+      "requiredTrophies": 0,
+      "onlineMembers": 0
+    },
+    "brawlers": [
+      {
+        "name": "Shelly",
+        "hasSkin": false,
+        "skin": null,
+        "trophies": 534,
+        "highestTrophies": 587,
+        "power": 8,
+        "rank": 21,
+        "rankImageUrl": "https://www.starlist.pro/assets/rank/21.png"
+      },
+      {
+        "name": "Colt",
+        "hasSkin": false,
+        "skin": null,
+        "trophies": 234,
+        "highestTrophies": 234,
+        "power": 6,
+        "rank": 13,
+        "rankImageUrl": "https://www.starlist.pro/assets/rank/13.png"
+      },
+      {
+        "name": "Bull",
+        "hasSkin": false,
+        "skin": null,
+        "trophies": 548,
+        "highestTrophies": 622,
+        "power": 7,
+        "rank": 22,
+        "rankImageUrl": "https://www.starlist.pro/assets/rank/22.png"
+      },
+      {
+        "name": "Brock",
+        "hasSkin": false,
+        "skin": null,
+        "trophies": 507,
+        "highestTrophies": 570,
+        "power": 7,
+        "rank": 21,
+        "rankImageUrl": "https://www.starlist.pro/assets/rank/21.png"
+      },
+      {
+        "name": "Rico",
+        "hasSkin": false,
+        "skin": null,
+        "trophies": 511,
+        "highestTrophies": 583,
+        "power": 10,
+        "rank": 21,
+        "rankImageUrl": "https://www.starlist.pro/assets/rank/21.png"
+      },
+      {
+        "name": "Barley",
+        "hasSkin": false,
+        "skin": null,
+        "trophies": 182,
+        "highestTrophies": 191,
+        "power": 5,
+        "rank": 12,
+        "rankImageUrl": "https://www.starlist.pro/assets/rank/12.png"
+      },
+      {
+        "name": "Jessie",
+        "hasSkin": false,
+        "skin": null,
+        "trophies": 559,
+        "highestTrophies": 622,
+        "power": 10,
+        "rank": 22,
+        "rankImageUrl": "https://www.starlist.pro/assets/rank/22.png"
+      },
+      {
+        "name": "Nita",
+        "hasSkin": false,
+        "skin": null,
+        "trophies": 87,
+        "highestTrophies": 87,
+        "power": 6,
+        "rank": 7,
+        "rankImageUrl": "https://www.starlist.pro/assets/rank/7.png"
+      },
+      {
+        "name": "Dynamike",
+        "hasSkin": false,
+        "skin": null,
+        "trophies": 455,
+        "highestTrophies": 480,
+        "power": 5,
+        "rank": 19,
+        "rankImageUrl": "https://www.starlist.pro/assets/rank/19.png"
+      },
+      {
+        "name": "El Primo",
+        "hasSkin": false,
+        "skin": null,
+        "trophies": 371,
+        "highestTrophies": 371,
+        "power": 5,
+        "rank": 16,
+        "rankImageUrl": "https://www.starlist.pro/assets/rank/16.png"
+      },
+      {
+        "name": "Mortis",
+        "hasSkin": false,
+        "skin": null,
+        "trophies": 252,
+        "highestTrophies": 255,
+        "power": 6,
+        "rank": 13,
+        "rankImageUrl": "https://www.starlist.pro/assets/rank/13.png"
+      },
+      {
+        "name": "Poco",
+        "hasSkin": false,
+        "skin": null,
+        "trophies": 79,
+        "highestTrophies": 80,
+        "power": 5,
+        "rank": 7,
+        "rankImageUrl": "https://www.starlist.pro/assets/rank/7.png"
+      },
+      {
+        "name": "Bo",
+        "hasSkin": false,
+        "skin": null,
+        "trophies": 531,
+        "highestTrophies": 578,
+        "power": 6,
+        "rank": 21,
+        "rankImageUrl": "https://www.starlist.pro/assets/rank/21.png"
+      },
+      {
+        "name": "Darryl",
+        "hasSkin": false,
+        "skin": null,
+        "trophies": 343,
+        "highestTrophies": 353,
+        "power": 4,
+        "rank": 16,
+        "rankImageUrl": "https://www.starlist.pro/assets/rank/16.png"
+      },
+      {
+        "name": "Penny",
+        "hasSkin": false,
+        "skin": null,
+        "trophies": 64,
+        "highestTrophies": 64,
+        "power": 5,
+        "rank": 6,
+        "rankImageUrl": "https://www.starlist.pro/assets/rank/6.png"
+      },
+      {
+        "name": "Tick",
+        "hasSkin": false,
+        "skin": null,
+        "trophies": 224,
+        "highestTrophies": 227,
+        "power": 5,
+        "rank": 13,
+        "rankImageUrl": "https://www.starlist.pro/assets/rank/13.png"
+      },
+      {
+        "name": "Rosa",
+        "hasSkin": false,
+        "skin": null,
+        "trophies": 487,
+        "highestTrophies": 495,
+        "power": 6,
+        "rank": 19,
+        "rankImageUrl": "https://www.starlist.pro/assets/rank/19.png"
+      },
+      {
+        "name": "Carl",
+        "hasSkin": false,
+        "skin": null,
+        "trophies": 57,
+        "highestTrophies": 58,
+        "power": 1,
+        "rank": 5,
+        "rankImageUrl": "https://www.starlist.pro/assets/rank/5.png"
+      },
+      {
+        "name": "Bibi",
+        "hasSkin": false,
+        "skin": null,
+        "trophies": 158,
+        "highestTrophies": 158,
+        "power": 2,
+        "rank": 10,
+        "rankImageUrl": "https://www.starlist.pro/assets/rank/10.png"
+      },
+      {
+        "name": "8-Bit",
+        "hasSkin": false,
+        "skin": null,
+        "trophies": 79,
+        "highestTrophies": 79,
+        "power": 1,
+        "rank": 6,
+        "rankImageUrl": "https://www.starlist.pro/assets/rank/6.png"
+      }
+    ],
+    "brawlersUnlocked": 20,
+    "highestBossFightLevel": 0,
+    "avatarId": 28000000,
+    "avatarUrl": "https://www.starlist.pro/assets/profile/28000000.png",
+    "hasSkins": false
+  };
